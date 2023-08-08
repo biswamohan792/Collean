96495f2a1f92a0e863b702531993683b21c647a7
Biswamohan Dwari <bmd097@gmail.com>
2023-08-08 21:28:35 +0530

    collean project init

---
+++ src/test/java/com/myHome/Collean/ColleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myHome.Collean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
